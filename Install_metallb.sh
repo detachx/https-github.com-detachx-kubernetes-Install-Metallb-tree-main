d772aae1428ba90c80d2c23e2f9cd8143cf35b40
detachx <46778884+detachx@users.noreply.github.com>
2023-04-30 01:59:03 +0200

    Update Install_metallb.sh

--- Install_metallb.sh
+++ Install_metallb.sh
@@ -1,5 +1,5 @@
 kubectl apply -f https://raw.githubusercontent.com/google/metallb/main/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/google/metallb/main/manifests/metallb.yaml
-kubectl apply -f https://github.com/detachx/kubernetes-Install-Metallb/blob/main/config.yaml
+kubectl apply -f https-github.com-detachx-kubernetes-Install-Metallb-tree-main/blob/main/Install_metallb.sh
 # On first install only
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
